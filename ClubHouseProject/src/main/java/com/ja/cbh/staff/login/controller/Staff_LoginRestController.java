012f1767d49b33e9fe3be6d52923521b256c6fef
Backgoms <102213232+Backgoms@users.noreply.github.com>
2022-10-04 21:31:36 +0900

    커밋시발

--- ClubHouseProject/src/main/java/com/ja/cbh/staff/login/controller/Staff_LoginRestController.java
+++ ClubHouseProject/src/main/java/com/ja/cbh/staff/login/controller/Staff_LoginRestController.java
@@ -1,8 +1,35 @@
 package com.ja.cbh.staff.login.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ja.cbh.staff.login.service.Staff_LoginRestService;
+import com.ja.cbh.vo.StaffVO;
+
 @RestController
+@RequestMapping("/staff/restLogin/*")
 public class Staff_LoginRestController {
 
+	@Autowired
+	private Staff_LoginRestService staff_LoginRestService;
+	
+	@RequestMapping("getStaffInfo")
+	public HashMap<String, Object> getStaffInfo(HttpSession session){
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		StaffVO staffVO = (StaffVO)session.getAttribute("sessionUserInfo");
+		
+		
+		
+		
+		map.put("session", staff_LoginRestService.getStaffByStaffId(staffVO.getStaff_id()));
+		
+		
+		return map;
+	}
 }
